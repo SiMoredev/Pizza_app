3795a35e2719c900b48cbf75d550b3d9de3eb90f
Сергей Климов <simore@MacBook-Air-Sergej.local>
2026-04-21 10:56:13 +0300

    Fix env

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from database import Base
 
 load_dotenv()
+
+#db
 
 app = FastAPI()
 
 @app.get("/get_pizzas")
 async def get_pizzas():
     return {"message": "Pizza API is alive!"}
+
+@app.post("/auth")
+async def auth_user():
+    pass
