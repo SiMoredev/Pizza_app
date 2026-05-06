55d4da76f5cd436e2f65462938fa9a77d8395285
SiMoredev <seryoga287@gmail.com>
2026-05-06 14:56:08 +0300

    add front html auth form

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -18,10 +18,8 @@ config = AuthXConfig(
     JWT_SECRET_KEY=os.getenv("JWT_SECRET_KEY"),
     JWT_ALGORITHM="HS256",
     JWT_ACCESS_TOKEN_EXPIRES=timedelta(minutes=30),
-    JWT_REFRESH_TOKEN_EXPIRES=timedelta(days=7),
     JWT_TOKEN_LOCATION=["cookies"],
     JWT_ACCESS_COOKIE_NAME="access_token",
-    JWT_REFRESH_COOKIE_NAME="refresh_token",
 )
 
 auth = AuthX(config=config)
